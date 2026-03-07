8f19f34c313d94172917f7316aa3200e55b3be9c
HT <hontmathiang@gmail.com>
2026-03-07 12:24:20 +0200

    adjust files

--- railway/start.sh
+++ railway/start.sh
@@ -7,4 +7,8 @@ export PORT="${PORT:-8080}"
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.rendered
 mv /etc/nginx/conf.d/default.conf.rendered /etc/nginx/conf.d/default.conf
 
+echo "=== NGINX CONFIG ==="
+cat /etc/nginx/conf.d/default.conf
+echo "=== END NGINX CONFIG ==="
+
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
